52a9f163069c8e16ebb8bd3cdcc34ca8f9058784
yashdobariya-scaletech <yash.dobariya@scaletech.xyz>
2022-01-07 19:02:07 +0530

    initial commit

---
+++ src/Shop.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Shop() {
+  return (
+    <div>
+      <h3>I Phone 13</h3>
+      <button className="btn btn-primary mx-2">-</button>
+      Add to Cart
+      <button className="btn btn-primary mx-2">+</button>
+    </div>
+  );
+}

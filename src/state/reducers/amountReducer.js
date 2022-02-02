1eda36ce58558401278794a3d6acd9088f701ea6
yashdobariya-scaletech <yash.dobariya@scaletech.xyz>
2022-02-02 13:01:10 +0530

    fix deposit money isuue

--- src/state/reducers/amountReducer.js
+++ src/state/reducers/amountReducer.js
@@ -1,6 +1,6 @@
 const reducer = (state = 0, action) => {
   if (action.type === "deposit") {
-    return state + action.payload;
+    return state - action.payload;
   } else if (action.type === "withdraw") {
     return state + action.payload;
   } else {
